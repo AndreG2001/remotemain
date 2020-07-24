ffc93537a7de159e1a9d41e26bcaf9ac4255954e
André Gonzaga <andregonzaga2001@gmail.com>
2020-07-24 18:35:55 -0300

    Creating a little text to know the password of database

---
+++ senha_banco_dados.sql
@@ -0,0 +1,4 @@
+insert into tabela (coluna, coluna, coluna) values (valor1, valor2, valor3)
+update tabela set coluna = 1 coluna = 2 where condition limit numberlimit
+delete from table where condition limit nuberlimit 
+tuncrate table
